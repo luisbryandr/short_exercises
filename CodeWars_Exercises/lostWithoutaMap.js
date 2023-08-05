ea89c73f06c9affaf90c3ddba10369290f9e7f65
luisbryandr <luisbdominguezr@gmail.com>
2023-08-05 16:21:30 -0400

    just a quick easy exercise to keep the consistency and momentum going in these daily to almost daily exercise to make coding a part of my identity

---
+++ CodeWars_Exercises/lostWithoutaMap.js
@@ -0,0 +1,14 @@
+/* 
+    Another quick warm up, to keep the momentum going.
+    Given an array of integers, return a new array with each value doubled.
+    For example:
+    [1, 2, 3] --> [2, 4, 6]
+    For the beginner, try to use the map method - it comes in very handy quite a lot so is a good one to know.
+
+    https://www.codewars.com/kata/beginner-lost-without-a-map/train/javascript
+    */
+    
+    function maps(x){
+        console.log(x.map(n => n * 2));
+        return x.map(n => n * 2);
+}

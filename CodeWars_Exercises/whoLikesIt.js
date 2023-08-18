46985e0eb207ed0d37ba976d294d5c8aafcb9275
luisbryandr <luisbdominguezr@gmail.com>
2023-08-17 20:34:13 -0400

    semi proud of this one, proud cause i made the time for a harder one ena dddid it on my own but dissapointed I had to break in to my family time in the evening

---
+++ CodeWars_Exercises/whoLikesIt.js
@@ -0,0 +1,26 @@
+/* 
+    Today I made time for a harder challenge today, it was an atypical day, family drama almost derailed my day but I had to do an atypical thing.  Take time off from my evening time with  my famly to get this exercise in.  a win is awin.
+
+    Description:  
+    
+    You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+Note: For 4 or more names, the number in "and 2 others" simply increases.
+*/
+
+function likes(names) {
+    return {
+        0: 'no one likes this',
+        1: `${names[0]} likes this`, 
+        2: `${names[0]} and ${names[1]} like this`, 
+        3: `${names[0]}, ${names[1]} and ${names[2]} like this`, 
+        4: `${names[0]}, ${names[1]} and ${names.length - 2} others like this`, 
+      }[Math.min(4, names.length)]
+}

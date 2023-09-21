6500b11f80b24e28038378910ea2e941fd3a768c
luisbryandr <luisbdominguezr@gmail.com>
2023-09-21 16:00:37 -0400

    here we go again, again and again till I reach my goal, not giving up

---
+++ CodeWars_Exercises/isItEven.js
@@ -0,0 +1,15 @@
+// back to start yet again,  not giving up though, starting over and over if I have to. going to reach my goal of landing a developer job. got dang it!
+
+// //In this Kata we are passing a number (n) into a function.
+
+// Your code will determine if the number passed is even (or not).
+
+// The function needs to return either a true or false.
+
+// Numbers may be positive or negative, integers or floats.
+
+// Floats with decimal part non equal to zero are considered UNeven for this kata.
+
+function testEven(n) {
+    return n % 2 === 0;
+}

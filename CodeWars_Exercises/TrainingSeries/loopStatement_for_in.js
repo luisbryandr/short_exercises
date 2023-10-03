19fbce5d9da9b19a0aafef2935b838c9f90fd5b7
luisbryandr <luisbdominguezr@gmail.com>
2023-10-02 21:15:01 -0400

    todays training and lesson, nice to learn the for in and for of variants of the for loop

---
+++ CodeWars_Exercises/TrainingSeries/loopStatement_for_in.js
@@ -0,0 +1,14 @@
+function giveMeFive(obj){
+    //coding here
+    let arr = [];
+    for (let key in obj){
+        if (key.length === 5){
+            arr.push(key);
+        }
+        if (obj[key].length === 5){
+            arr.push(obj[key]);
+        }
+    }
+    return arr;
+  }
+

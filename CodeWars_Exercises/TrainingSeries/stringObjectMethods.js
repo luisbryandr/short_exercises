dfb3ff2d97dcc28ed7d2d2f7eae268563c5a4354
luisbryandr <luisbdominguezr@gmail.com>
2023-10-09 21:45:39 -0400

    didn't commit all my changes and latest save on last commit

--- CodeWars_Exercises/TrainingSeries/stringObjectMethods.js
+++ CodeWars_Exercises/TrainingSeries/stringObjectMethods.js
@@ -1,9 +1,21 @@
 function cutIt(arr){
-    //coding here...
-    let sortedArr = arr.sort((a, b) => a.length - b.length)
-    let shortestStrLength = sortedArr[0].length;
+    let shortestStrLength = arr[0].length;
     for(let i = 0; i < arr.length; i++) {
-     arr[i] = arr[i].substr(0,shortestStrLength)
-    } 
+        if(arr[i].length < shortestStrLength) {
+            shortestStrLength = arr[i].length;
+        }
+    }
+    for(let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].substr(0,shortestStrLength)
+    }
     return arr;
   }
+
+
+
+//   let sortedArr = arr.sort((a, b) => a.length - b.length)
+//   let shortestStrLength = sortedArr[0].length;
+//   for(let i = 0; i < arr.length; i++) {
+//    arr[i] = arr[i].substr(0,shortestStrLength)
+//   } 
+//   return arr;

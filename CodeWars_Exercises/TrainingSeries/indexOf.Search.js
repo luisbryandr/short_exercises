648a995e5fc21d63cdbbb3b380c8f268091584fe
luisbryandr <luisbdominguezr@gmail.com>
2023-10-14 14:34:14 -0400

    another lesson to try and not go too many days without learning or touching code.  might have to pivot to a different job in tech for a while but goal is to keep learning and mastering part time

---
+++ CodeWars_Exercises/TrainingSeries/indexOf.Search.js
@@ -0,0 +1,8 @@
+function firstToLast(str,c){
+    let first = str.indexOf(c)
+    let last = str.lastIndexOf(c)
+    return first === -1 ? first : last - first
+}
+
+//nice to learn and see that co pilot can do this so easily and withouth prompting
+// still learning the fundamentals of JS and how to use it but its good to know that I don't have to memorize everything and that I can use co pilot to help me

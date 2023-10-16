a9acb7f7ad9c601f76daaf4219ecd106990d6367
luisbryandr <luisbdominguezr@gmail.com>
2023-10-15 22:39:44 -0400

    kinda discouraging I couldn't get this one even with console log but Im glad I was able to see the scope of map and each use of join and split in the given answer slash code'

---
+++ CodeWars_Exercises/TrainingSeries/split_join.js
@@ -0,0 +1,10 @@
+function splitAndMerge(str, sp) {
+    return str.split(' ').map(word => word.split('').join(sp)).join(' ')
+}
+
+// "tried to do this on my own, using console.log to see what was happening."
+//but I couldn't get it to work. I had the right idea, but I couldn't get it to work.
+//finally just used copilot to get the answer. I need to study this one more.
+//I'm not sure I understand how it works.
+//I kinda of get it after typing it out but I need more practice and refresher with map and join. 
+//I mean i see the way and the scope of map and join and split  as a function of the arrays within the array but def need more practice.

954f4491d627fca81c7413b18968fef116078de0
luisbryandr <luisbdominguezr@gmail.com>
2023-10-25 21:01:40 -0400

    console logs from yesterday

--- CodeWars_Exercises/sumOfOddNumbers.js
+++ CodeWars_Exercises/sumOfOddNumbers.js
@@ -1,3 +1,7 @@
 function rowSumOddNumbers(n) {
   return Math.pow(n, 3);
 }
+
+console.log(rowSumOddNumbers(1)); // 1  
+console.log(rowSumOddNumbers(2)); // 8  
+console.log(rowSumOddNumbers(3)); // 27

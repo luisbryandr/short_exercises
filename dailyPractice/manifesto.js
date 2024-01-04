942a7d30cc4869cd665be11366d6a619034ec3a6
luisbryandr <luisbdominguezr@gmail.com>
2024-01-04 15:01:04 -0500

    update to manifesto, writing down goals

--- dailyPractice/manifesto.js
+++ dailyPractice/manifesto.js
@@ -1,1 +1,4 @@
-/* just a place for me to record my daily code practice */
+/*  a place for me to record my daily code practice 
+        Aim: 1 hour a day, 5 days a week
+        Secondary aim: 5 mins a day, 7 days a week
+*/

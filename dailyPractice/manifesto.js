3c0f4a6020fbd2be1f616a375a7028cfa9cd46b7
luisbryandr <luisbdominguezr@gmail.com>
2024-01-06 17:38:17 -0500

    today's coding exercise, contains how i fell in love with coding again

--- dailyPractice/manifesto.js
+++ dailyPractice/manifesto.js
@@ -2,3 +2,7 @@
         Aim: 1 hour a day, 5 days a week
         Secondary aim: 5 mins a day, 7 days a week
 */
+
+// 2021-01-04
+// 1 hour - did a yesterday's codewars challenge on my own to practice and then did a lesson from BloomTech codex
+// link to my latest reason for pursuing web development --hint hint -- why i feel in love with it  https://codepen.io/Lbdcreador/pen/QWoNMrm
